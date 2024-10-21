35db20960c7f89906303f24ecce570f9c92db454
onebottlekick <gksqudcks97@gmail.com>
2024-10-21 13:27:22 +0900

    add: [onebottlekick] example for chat_chain use case

---
+++ examples/chain.py
@@ -0,0 +1,11 @@
+from chat_chain.service.chat_chain_service_impl import ChatChainServiceImpl
+
+
+if __name__ == "__main__":
+    chain = ChatChainServiceImpl(
+        task_prompt="Develop a calculator app using python and tkinter",
+        directory="test_dir",
+        base_url="https://si-follow.loca.lt/v1/",
+        model="llama3.2",
+    )
+    chain.run()
