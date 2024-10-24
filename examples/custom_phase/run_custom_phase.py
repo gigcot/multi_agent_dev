bcf0a805e5206d11c3c46975b8f5ca5c8b5824ca
onebottlekick <gksqudcks97@gmail.com>
2024-10-24 09:44:12 +0900

    chore: [onebottlekick] Updated example for running custom phase

--- examples/custom_phase/run_custom_phase.py
+++ examples/custom_phase/run_custom_phase.py
@@ -1,15 +1,9 @@
 from examples.custom_phase.example_phase import *
-from made.phase.service.phase_service_impl import PhaseServiceImpl
-from made.engine.entity.ollama_config import OllamaConfig
-from made.chat_env.entity.env_config import EnvConfig
-from made.chat_env.repository.chat_env_repository_impl import ChatEnvRepositoryImpl
+from made.chat_chain.service.chat_chain_service_impl import ChatChainServiceImpl
 
 
-model_config = OllamaConfig(base_url="https://si-follow.loca.lt/v1/")
-phase_service = PhaseServiceImpl(model_config=model_config)
-env = ChatEnvRepositoryImpl(EnvConfig(task_prompt="discuss"))
-custom_phase = phase_service.get_phase(
-    phase_name="Example",
+chain = ChatChainServiceImpl(
+    task_prompt="Discuss about LLM multi agents.", directory="test", phases=["Example"]
 )
 
-custom_phase.execute(env=env)
+chain.run()
