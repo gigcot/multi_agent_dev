9447698b54d3925e13c51f297351aa6c558bd909
onebottlekick <gksqudcks97@gmail.com>
2024-10-25 12:10:03 +0900

    feat: [onebottlekick] File management tool

---
+++ made/tools/file/repository/file_tool_repository.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+class FileToolRepository(ABC):
+    @abstractmethod
+    def create_empty_file(file_path):
+        pass
+
+    @abstractmethod
+    def read_file(file_path):
+        pass
+
+    @abstractmethod
+    def write_file(file_path, content):
+        pass
+
+    @abstractmethod
+    def update_file(file_path, content):
+        pass
+
+    @abstractmethod
+    def delete_file(file_path):
+        pass
+
+    @abstractmethod
+    def get_file_name_from_text(text: str, regex: str = r"(\w+\.\w+)"):
+        pass
+
+    @abstractmethod
+    def get_contents_from_text(text, regex):
+        pass
+
