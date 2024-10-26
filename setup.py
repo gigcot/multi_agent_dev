25b2ce5572c76e025a27be282d495d7581321fa8
onebottlekick <gksqudcks97@gmail.com>
2024-10-27 00:37:34 +0900

    chore: [onebottlekick] Updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="llm_made",
-    version="0.0.5",
+    version="0.0.6",
     description="multi-agent development",
     author="onebottlekick",
     author_email="gksqudcks97@gmail.com",
