39ba4a0a5b8629d4291cc1c23868b3827de8df3e
onebottlekick <gksqudcks97@gmail.com>
2024-11-04 10:07:43 +0900

    feat: [onebottlekick] Added phase conversation logging

--- made/phase/repository/base_phase_repository_impl.py
+++ made/phase/repository/base_phase_repository_impl.py
@@ -1,16 +1,19 @@
-from copy import deepcopy
 import os
-from typing import Dict, Union, Optional
+from copy import deepcopy
+from typing import Dict, Optional, Union
 
 from made.agent.service.agent_service_impl import AgentServiceImpl
 from made.chat_env.repository.chat_env_repository_impl import ChatEnvRepositoryImpl
-from made.messages.entity.chat_message.user_message import UserChatMessage
-from made.messages.entity.system_message.assistant_system_message import AssistantSystemMessage
 from made.engine import ModelConfig
+from made.messages.entity.chat_message.user_message import UserChatMessage
+from made.messages.entity.system_message.assistant_system_message import (
+    AssistantSystemMessage,
+)
 from made.phase.entity.phase_states import PhaseStates
 from made.phase.repository.base_phase_repository import BasePhaseRepository
 from made.role_playing.service.role_playing_service_impl import RolePlayingServiceImpl
 from made.tools.rag.repository.rag_tool_repository_impl import RAGToolRepositoryImpl
+from made.utils.logger import Logger
 
 
 class BasePhaseRepositoryImpl(BasePhaseRepository):
@@ -24,6 +27,7 @@ class BasePhaseRepositoryImpl(BasePhaseRepository):
         user_role_prompt: str,
         chat_turn_limit: int = 10,
         conversation_rag: bool = False,
+        conversation_logging: bool = True,
         **kwargs,
     ):
         model_config = deepcopy(model_config)
@@ -41,6 +45,7 @@ class BasePhaseRepositoryImpl(BasePhaseRepository):
         self.user_role_prompt = user_role_prompt
         self.chat_turn_limit = chat_turn_limit
         self.conversation_rag = conversation_rag
+        self.conversation_logging = conversation_logging
 
         self.seminar_conclusion = None
 
@@ -67,6 +72,15 @@ class BasePhaseRepositoryImpl(BasePhaseRepository):
             placeholders = placeholders.__dict__
         assert 1 <= chat_turn_limit <= 100
 
+        conversation_logger = None
+        if self.conversation_logging:
+            conversation_logger = Logger(
+                f"{__class__.__name__}_{env.config.directory}",
+                os.path.join(
+                    env.config.directory, "logs", f"{ self.__class__.__name__}.log"
+                ),
+            ).get_logger()
+
         role_play_session = RolePlayingServiceImpl(
             model_config=self.model_config,
             task_prompt=task_prompt,
@@ -80,31 +94,43 @@ class BasePhaseRepositoryImpl(BasePhaseRepository):
         _, input_user_message = role_play_session.role_playing_repository.init_chat(
             placeholders, phase_prompt
         )
-        print()
-        print(
-            f"\033[93m{input_user_message.role_name}\033[0m",
-            ": ",
-            input_user_message.content,
-        )
+        # print()
+        # print(
+        #     f"\033[93m{input_user_message.role_name}\033[0m",
+        #     ": ",
+        #     input_user_message.content,
+        # )
+        if conversation_logger is not None:
+            conversation_logger.info(
+                f"{input_user_message.role_name}: {input_user_message.content}"
+            )
         seminar_conclusion = None
 
         for _ in range(chat_turn_limit):
             assistant_response, user_response = role_play_session.step(
                 input_user_message, chat_turn_limit == 1
             )
-            print()
-            print(
-                f"\033[93m{assistant_response.message.role_name}\033[0m",
-                ": ",
-                assistant_response.message.content,
-            )
-            if user_response.message is not None:
-                print()
-                print(
-                    f"\033[93m{user_response.message.role_name}\033[0m",
-                    ": ",
-                    user_response.message.content,
+            # print()
+            # print(
+            #     f"\033[93m{assistant_response.message.role_name}\033[0m",
+            #     ": ",
+            #     assistant_response.message.content,
+            # )
+            if conversation_logger is not None:
+                conversation_logger.info(
+                    f"{assistant_response.message.role_name}: {assistant_response.message.content}"
                 )
+            if user_response.message is not None:
+                # print()
+                # print(
+                #     f"\033[93m{user_response.message.role_name}\033[0m",
+                #     ": ",
+                #     user_response.message.content,
+                # )
+                if conversation_logger is not None:
+                    conversation_logger.info(
+                        f"{user_response.message.role_name}: {user_response.message.content}"
+                    )
             if (
                 role_play_session.role_playing_repository.assistant_agent.agent_repository.info
             ):
@@ -133,15 +159,30 @@ class BasePhaseRepositoryImpl(BasePhaseRepository):
                     role_play_session.role_playing_repository.assistant_agent.agent_repository.stored_messages
                 )
                 task = conversations[0].content
-                conversations = [conversation.content for conversation in conversations[1:]]
-                client = RAGToolRepositoryImpl.get_client(os.path.join(env.config.directory, "vectordb"))
-                collection = RAGToolRepositoryImpl.get_collection(client, "conversations")
-                RAGToolRepositoryImpl.ingest(collection, conversations, self.model_config)
-                search_result = RAGToolRepositoryImpl.search(task, collection, self.model_config, n_results=2)
-                rag_prompt = f"Query search result: {search_result}\n" + f"Using the result, do {task}."
+                conversations = [
+                    conversation.content for conversation in conversations[1:]
+                ]
+                client = RAGToolRepositoryImpl.get_client(
+                    os.path.join(env.config.directory, "vectordb")
+                )
+                collection = RAGToolRepositoryImpl.get_collection(
+                    client, "conversations"
+                )
+                RAGToolRepositoryImpl.ingest(
+                    collection, conversations, self.model_config
+                )
+                search_result = RAGToolRepositoryImpl.search(
+                    task, collection, self.model_config, n_results=2
+                )
+                rag_prompt = (
+                    f"Query search result: {search_result}\n"
+                    + f"Using the result, do {task}."
+                )
                 message = UserChatMessage(content=task)
                 agent = AgentServiceImpl(
-                    system_message=AssistantSystemMessage(role_name=assistant_role_name, content=rag_prompt),
+                    system_message=AssistantSystemMessage(
+                        role_name=assistant_role_name, content=rag_prompt
+                    ),
                     model_config=self.model_config,
                 )
                 seminar_conclusion = agent.step(message).message.content
@@ -151,8 +192,10 @@ class BasePhaseRepositoryImpl(BasePhaseRepository):
                 seminar_conclusion = "<INFO>" + assistant_response.message.content
 
         seminar_conclusion = seminar_conclusion.split("<INFO>")[-1]
-        print()
-        print(f"\033[31mseminar conclusion\033[0m: {seminar_conclusion}")
+        # print()
+        # print(f"\033[31mseminar conclusion\033[0m: {seminar_conclusion}")
+        if conversation_logger is not None:
+            conversation_logger.info(f"[seminar conclusion]: {seminar_conclusion}")
         return seminar_conclusion
 
     def update_phase_states(self, env: ChatEnvRepositoryImpl):
